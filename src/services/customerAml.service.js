439a722ec09f21f20fd95087c7d5c93b76e486f7
Наталія Лех <nlekh@impressit.io>
2022-09-22 15:16:12 +0300

    GWAD-12: fixes after tests

--- src/services/customerAml.service.js
+++ src/services/customerAml.service.js
@@ -2,10 +2,10 @@ import Request from "../helper/request"
 
 const request = new Request()
 
-const listAml = async (identityStatusId, isgwMonitored) => {
+const listAml = async (identityStatusId, isGWMonitored) => {
     return await request.postRequest(`/admin/customers/aml-list/`, {
         identityStatusId: identityStatusId,
-        isgwMonitored:isgwMonitored
+        isGWMonitored:isGWMonitored
     })
 }
 
