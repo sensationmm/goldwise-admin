28e274e2fc046c8c76c24af750883d0bb3c8caed
Kevin Reynolds <kevin@sensationmultimedia.co.uk>
2025-05-09 13:12:55 -0700

    Toggle styling

--- src/components/atoms/ActiveToggle/ActiveToggle.js
+++ src/components/atoms/ActiveToggle/ActiveToggle.js
@@ -4,7 +4,7 @@ const ActiveToggle = ({isActive = true}) => {
   const label = isActive ? 'On': 'Off';
   const color  = isActive ? 'indigo-400': 'gray-400';
 
-  return <div className={`flex flex-row${!isActive && '-reverse'} gap-[5px] items-center text-${color} font-normal`}>
+  return <div className={`inline-flex flex-row${!isActive && '-reverse'} gap-[5px] items-center text-${color} font-normal bg-gray-200 rounded-[10px] p-[0px_3px]`}>
     {label}
     <div className={`w-[14px] h-[14px] rounded-[7px] bg-${color}`} />
   </div>
