1158c1798e6ed608e7b6cb7ef5d66fbcf996efe4
Kevin Reynolds <kevin@sensationmultimedia.co.uk>
2025-05-09 13:38:53 -0700

    tweak

--- src/components/atoms/ActiveToggle/ActiveToggle.js
+++ src/components/atoms/ActiveToggle/ActiveToggle.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 const ActiveToggle = ({isActive = true}) => {
   const label = isActive ? 'On': 'Off';
-  const color  = isActive ? 'indigo-400': 'gray-400';
+  const bg  = isActive ? 'bg-indigo-400': 'bg-gray-400';
+  const text  = isActive ? 'text-indigo-400': 'text-gray-400';
 
-  return <div className={`inline-flex flex-row${!isActive && '-reverse'} gap-[5px] items-center text-${color} font-normal bg-gray-200 rounded-[10px] p-[0px_5px]`}>
+  return <div className={`inline-flex flex-row${!isActive && '-reverse'} gap-[5px] items-center ${text} font-normal bg-gray-200 rounded-[10px] p-[0px_5px]`}>
     {label}
-    <div className={`w-[14px] h-[14px] rounded-[7px] bg-${color}`} />
+    <div className={`w-[14px] h-[14px] rounded-[7px] ${bg}`} />
   </div>
 }
 
